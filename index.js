cf3eb83b093da03d436d749bd052a1e739c340ca
Amit <133666068+Garzzzza@users.noreply.github.com>
2023-07-11 21:45:52 +0300

    To Merge

--- index.js
+++ index.js
@@ -15,3 +15,32 @@ function getLocation() {
 }
 
 getLocation();
+const galleryContainer = document.getElementById("gallery");
+const images = galleryContainer.getElementsByTagName("img");
+
+// Add click event listener to each image
+for (let i = 0; i < images.length; i++) {
+  images[i].addEventListener("click", function () {
+    displayImage(i);
+  });
+}
+
+// Function to display clicked image in a larger view
+function displayImage(index) {
+  // Create a modal or lightbox to display the image in a larger view
+  // Example: Create a modal div and append the clicked image to it
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+
+  const image = document.createElement("img");
+  image.src = images[index].src;
+  image.alt = images[index].alt;
+
+  modal.appendChild(image);
+  document.body.appendChild(modal);
+
+  // Add event listener to close the modal when clicked
+  modal.addEventListener("click", function () {
+    modal.remove();
+  });
+}
