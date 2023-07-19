03da33acb6c99901bae1fd47ce97ab001ba7c580
Lukas Lehrmann <121244965+leoLule@users.noreply.github.com>
2023-07-19 13:01:50 +0300

    test

--- index.js
+++ index.js
@@ -78,9 +78,19 @@ const outputNote = document.getElementById("output-note");
 
 function addFlora() {
   const flora = grabFlora.value;
-  const divElement = document.createElement("div");
-  divElement.className = "myDiv";
-  divElement.innerHTML = flora;
+
+  // const divElement = document.createElement("div");
+  // divElement.className = "myDiv";
+  // divElement.innerHTML = flora;
+  const card = `<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`;
+  gallery.innerHTML += card;
   floraData.push({
     flora: flora,
   });
