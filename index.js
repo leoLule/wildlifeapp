5d2e9c7c893e060f7cfb9506fc1d17f88238ceae
Amit <133666068+Garzzzza@users.noreply.github.com>
2023-07-22 23:12:26 +0300

    finishings + pictures

--- index.js
+++ index.js
@@ -106,14 +106,12 @@ function addImage(parentElement) {
   localStorage.setItem("savedImageURLs", JSON.stringify(imageURLs));
 }
 
-//on submit/click
 grabSubmit.addEventListener("click", function () {
   const experienceDiv = addExperienceDiv();
   addImage(experienceDiv);
   addFlora(experienceDiv);
   addLocation(experienceDiv);
   addNote(experienceDiv);
-  // gallery.innerHTML = ""; if we want to empty the existing gallery
 });
 function displayFromStorage() {
   if (savedFloraData !== null) {
@@ -156,30 +154,3 @@ function displayFromStorage() {
 window.addEventListener("DOMContentLoaded", function () {
   displayFromStorage()
 });
-
-// // Add click event listener to each image
-// for (let i = 0; i < images.length; i++) {
-//   images[i].addEventListener("click", function () {
-//     displayImage(i);
-//   });
-// }
-
-// // Function to display clicked image in a larger view
-// function displayImage(index) {
-//   // Create a modal or lightbox to display the image in a larger view
-//   // Example: Create a modal div and append the clicked image to it
-//   const modal = document.createElement("div");
-//   modal.classList.add("modal");
-
-//   const image = document.createElement("img");
-//   image.src = images[index].src;
-//   image.alt = images[index].alt;
-
-//   modal.appendChild(image);
-//   document.body.appendChild(modal);
-
-//   // Add event listener to close the modal when clicked
-//   modal.addEventListener("click", function () {
-//     modal.remove();
-//   });
-// }
